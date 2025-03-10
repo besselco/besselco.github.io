89535103569229d2c469ab806da691cd5a989605
besselco <web@besselco.com>
2025-03-10 10:27:09 -0500

    Update About.style.ts

--- src/views/About/About.style.ts
+++ src/views/About/About.style.ts
@@ -476,7 +476,7 @@ export const AboutSubLogoC24 = styled.img`
     transition: all 0.3s ease-in-out;
 
 `
-export const AboutSubLogoBDI = styled.img`
+export const AboutSubLogoBDIS = styled.img`
     width: 35%;
     &:hover {
         transform: scale(1.2);
