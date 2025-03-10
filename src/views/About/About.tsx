e58b39e9e57b9ad3fe8d84fe8f64c1910ccfa5e0
besselco <web@besselco.com>
2025-03-10 10:09:31 -0500

    Update About.tsx

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -26,7 +26,7 @@ import BesselLogo from "assets/images/BesselCo-Logo.png";
 import CentellianLogo from "assets/images/centellian24_logo.png";
 import MadecaLogo from "assets/images/Madeca_Derma_Logo_Icon_blk.png";
 import WDLogo from "assets/images/wdressroom_logo_blk1.png";
-import BDILogo from "assets/images/BDI.png";
+import BDILogo from "assets/images/BDIS_logo.png";
 import { useInView } from "react-intersection-observer";
 
 const About = () => {
@@ -98,7 +98,7 @@ const About = () => {
             <AboutSubLogoC24 src={CentellianLogo} />
           </AboutSubLogoContainer>
 
-          <AboutSubLogoContainer href="/">
+          <AboutSubLogoContainer href="https://www.bdidist.com">
             <AboutSubLogoBDI src={BDILogo} />
           </AboutSubLogoContainer>
         </AboutSubLogoGroupContainer>
