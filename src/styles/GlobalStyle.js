49ddeeb1dcaf18a05699b76d0265b6721d1d8275
besselco <web@besselco.com>
2025-03-18 15:03:22 -0500

    Update GlobalStyle.js

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        font-family: "Poppins";
+        font-family: "Hevetica";
         font-size: 14px;
         font-weight: 400;
         margin: 0;
